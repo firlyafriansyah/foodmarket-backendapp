9fbb85be94c9b17a32ea06d71c3ba0160d4d7e78
Firly <firlyafriansyah50@gmail.com>
2021-11-02 06:54:42 +0700

    Make Update Photo User Controller

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -91,7 +92,7 @@ class UserController extends Controller
     {
         $token = $request->user()->currentAccessToken()->delete();
 
-        return ResponseFormatter::succes($token, 'Token Revoked');
+        return ResponseFormatter::success($token, 'Token Revoked');
     }
 
     public function fetch(Request $request)
@@ -107,5 +108,30 @@ class UserController extends Controller
         $user->update($data);
 
         return ResponseFormatter::success($user, 'Profile Updated');
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|image|max:2048'
+        ]);
+
+        if($validator->fails()) {
+            return ResponseFormatter::error([
+                'error' => $validator->errors(),
+            ], 'Update photo fails', 401);
+        }
+
+        if($request->file('file')) {
+            $file = $request->file->store('assets/user', 'public');
+
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([
+                $file
+            ], 'File successfuly uploaded');
+        }
     }
 }
