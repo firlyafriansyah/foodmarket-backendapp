4d64adf14e772de524a6bb5cc7acebd65b23085e
Firly <firlyafriansyah50@gmail.com>
2021-11-24 15:33:33 +0700

    Slicing web dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,11 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin-dashboard');
 });
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+Route::prefix('dashboard')->middleware(['auth:sanctum', 'admin'])->group(function() {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+});
 
 Route::get('midtrans/success', [MidtransController::class, 'success']);
 Route::get('midtrans/unfinish', [MidtransController::class, 'unfinish']);
