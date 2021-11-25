4a1d31bd46973c6461c72580e64d050d6c9b9cbb
Firly <firlyafriansyah50@gmail.com>
2021-11-25 15:24:48 +0700

    make user list on dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\MidtransController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,11 +17,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('admin-dashboard');
+    return redirect()->route('dashboard');
 });
 
 Route::prefix('dashboard')->middleware(['auth:sanctum', 'admin'])->group(function() {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('users', UserController::class);
 });
 
 Route::get('midtrans/success', [MidtransController::class, 'success']);
